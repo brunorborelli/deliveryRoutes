e88c2a2d2494cadede2d1c62bd27c7c1bca9ccf7
Bruno Borelli <rodriguesborellibruno@gmail.com>
2024-11-20 10:55:23 -0300

    #001 - Esqueleto do projeto

---
+++ src/test/java/com/engsoft/marmita/MarmitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engsoft.marmita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarmitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
