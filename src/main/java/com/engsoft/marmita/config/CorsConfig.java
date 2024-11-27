dc4bf8ddf6b69b52d819b836c2e1cb1865b68d93
Geydson <geydson87@gmail.com>
2024-11-26 21:26:11 -0300

    feat liberar origin

--- src/main/java/com/engsoft/marmita/config/CorsConfig.java
+++ src/main/java/com/engsoft/marmita/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("OPTIONS");
         config.addAllowedMethod("GET");
