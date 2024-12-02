a5da4f2c789df4178e901ce28d2a7520cff898ea
Geydson <68717248+Geydson-Santos@users.noreply.github.com>
2024-12-01 23:49:03 -0300

    Update CorsConfig.java

--- src/main/java/com/engsoft/marmita/config/CorsConfig.java
+++ src/main/java/com/engsoft/marmita/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins("*");
+        config.setAllowedOrigins(Arrays.asList("*"));
         config.addAllowedHeader("*");
         config.addAllowedMethod("OPTIONS");
         config.addAllowedMethod("GET");
